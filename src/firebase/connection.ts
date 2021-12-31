f4b9c1ced4054a50c5cc7105a3e69842a06a103a
raoni111 <raoni.itar@gmail.com>
2021-12-31 18:46:27 -0300

    my first commit

---
+++ src/firebase/connection.ts
@@ -0,0 +1,49 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, child, get } from 'firebase/database';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+const _apiKey = process.env.REACT_APP_APIKEY;
+const _authDomain = process.env.REACT_APP_AUTHDOMAIN;
+const _databaseURL = process.env.REACT_APP_DATABASEURL;
+const _projectId = process.env.REACT_APP_PROJECTID;
+const _storageBucket = process.env.REACT_APP_STOREGEBUCKET;
+const _messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID;
+const _appId = process.env.REACT_APP_APPID;
+const _measurementId = process.env.REACT_APP_MEASUREMENTID;
+
+const firebaseConfig = {
+  apiKey: _apiKey,
+  authDomain: _authDomain,
+  databaseURL: _databaseURL,
+  projectId: _projectId,
+  storageBucket: _storageBucket,
+  messagingSenderId: _messagingSenderId,
+  appId: _appId,
+  measurementId: _measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+function writeUserData(
+  userId: string,
+  name: string,
+  email: string,
+  imageUrl: string,
+): void {
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+}
+writeUserData('123', 'Raoni', 'raoni.itar@gmail.com', 'imageAleatoria.png');
+
+const bdRef = ref(db);
+get(child(bdRef, 'users/12321d1223'))
+  .then((response) => {
+    console.log(response.val());
+  })
+  .catch((error) => {
+    console.log(error);
+  });
