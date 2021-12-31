935c4229e8bf3dd9904455bece8c5e794b6fd6a1
raoni111 <raoni.itar@gmail.com>
2021-12-31 20:00:14 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Helo World</h1>
+      <Login />
     </div>
   );
 }
